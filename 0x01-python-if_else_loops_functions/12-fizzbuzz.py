8a08c3f0a6550a07044b12c5ddba03a94248e32f
Nedu2022 <nwabuokeichinedu00@gmail.com>
2022-11-30 00:13:24 +0100

    if_else_loops_functions

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-# Author - Nwabuokei Chinedu
-"""Print the numbers from 1 to 100 separated by a space.
-  For multiples of three, print Fizz instead of the number
-  For multiples of five, print Buzz instead of the number.
-  For multiples of three and five, print FizzBuzz instead of the number.
-  """
+FIZZ = "Fizz"
+BUZZ = "Buzz"
 
 
 def fizzbuzz():
