1228aa514d18111604acf0663369036830cfbcc2
Nedu2022 <nwabuokeichinedu00@gmail.com>
2023-04-26 02:29:14 +0100

    starwars

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,9 +1,4 @@
 #!/usr/bin/node
-const request = require('request');
 const fs = require('fs');
-
-request(process.argv[2], function (err, response, body) {
-  if (err == null) {
-    fs.writeFileSync(process.argv[3], body);
-  }
-});
+const request = require('request');
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
